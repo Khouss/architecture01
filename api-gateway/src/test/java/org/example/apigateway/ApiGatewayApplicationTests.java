2a769f30aed6b252ad605ab302d0f253cbc8c83a
Khouss <oussamakhedif82@gmail.com>
2025-12-17 23:36:35 +0100

    3

--- api-gateway/src/test/java/org/example/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/org/example/apigateway/ApiGatewayApplicationTests.java
@@ -1,11 +1,24 @@
 package org.example.apigateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void healthEndpointShouldReturn200() throws Exception {
+        mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk());
+    }
 
 }
